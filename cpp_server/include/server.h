24dc838be914df2214e24bb815f1ce3c515b0911
Sergey Golubev <seregey.golubev@gmail.com>
2020-11-28 19:03:59 +0300

    cpp_server

---
+++ cpp_server/include/server.h
@@ -0,0 +1,1 @@
+#ifndef SERVER_SERVER_H#define SERVER_SERVER_H#include <iostream>#include <sys/types.h>#include <sys/socket.h>#include <netinet/in.h>#include <unistd.h>class Server {public:    unsigned short port = 1234;    Server();    ~Server();    void startServer();};#endif //SERVER_SERVER_H
