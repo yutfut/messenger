aff974a367b0c65fdde9d1f5a34734befe238463
Aliev2000 <alievz2011@gmail.com>
2020-12-08 20:32:11 +0300

    Authorization initial commit

---
+++ authorization/include/authorization.h
@@ -0,0 +1,8 @@
+//
+// Created by parviz on 08.12.2020.
+//
+
+#ifndef MESSENGER_AUTHORIZATION_H
+#define MESSENGER_AUTHORIZATION_H
+
+#endif //MESSENGER_AUTHORIZATION_H
---
+++ authorization/src/authorization.cpp
@@ -0,0 +1,4 @@
+//
+// Created by parviz on 08.12.2020.
+//
+
